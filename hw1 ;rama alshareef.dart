0c3d2455e40ffd8c100941572b60ff960802d800
Rama774 <105730518+Rama774@users.noreply.github.com>
2022-05-18 22:24:37 +0300

    Add files via upload

---
+++ hw1 ;rama alshareef.dart
@@ -0,0 +1,12 @@
+void main() {
+ const name =("rama Alshareef");
+   var dateofbirth =("june19");
+   var mobilenumber =("539090442");
+  //the number is saudi
+  
+  print("my name is ${name} and my birthday ${dateofbirth} my mobile number is ${mobilenumber}");
+  print(mobilenumber[4]);
+  
+  print(name.length);
+    
+}
